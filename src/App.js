b56d3eab412f7c5d2bffe6bc12a7bc2c1de6d187
Your Name <you@example.com>
2022-06-26 20:00:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar />
+
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/shop' element={<Shop />} />
+      <Route exact path='/about' element={<About />} />
+      <Route exact path='/contact' element={<Contact />} />
+    </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
